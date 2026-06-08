9fbfdac1f3262a8f9a4d2dd233b9071a01fa00ca
gritsruslan <gritsruslan@gmail.com>
2026-06-08 12:39:33 +0300

    backend: add swagger

--- backend/Microsoft.ToDo.API/Program.cs
+++ backend/Microsoft.ToDo.API/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
