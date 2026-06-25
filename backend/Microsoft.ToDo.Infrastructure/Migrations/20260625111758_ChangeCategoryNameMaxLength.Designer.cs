00efac515e16ffbb050cefc25146501dabefd1a8
gritsruslan <gritsruslan@gmail.com>
2026-06-25 14:18:33 +0300

    backend: migration for categories

--- backend/Microsoft.ToDo.Infrastructure/Migrations/20260625111758_ChangeCategoryNameMaxLength.Designer.cs
+++ backend/Microsoft.ToDo.Infrastructure/Migrations/20260625111758_ChangeCategoryNameMaxLength.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.ToDo.Infrastructure;
 
@@ -11,9 +12,11 @@ using Microsoft.ToDo.Infrastructure;
 namespace Microsoft.ToDo.Infrastructure.Migrations
 {
     [DbContext(typeof(ToDoDbContext))]
-    partial class ToDoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260625111758_ChangeCategoryNameMaxLength")]
+    partial class ChangeCategoryNameMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Microsoft.ToDo.Infrastructure/Migrations/20260625111758_ChangeCategoryNameMaxLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microsoft.ToDo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCategoryNameMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
